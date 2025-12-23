5065561967c61d74cc67b5e17a1947a23e8f735d
sarahkessii <sarxhkessi@gmail.com>
2025-12-22 19:21:33 -0500

    Fix CSS Menu, fix title accessiblite, ajout des noms

--- analyse.js
+++ analyse.js
@@ -1,3 +1,4 @@
+// Sarah Kessi - 20348702, Felicia Luu - 22334856, Marie Makarenkov 20342056, Kenza Hilali 20205908
 console.log("Titre de la page: ", document.title);
 console.log("URL de la page: ", window.location.href);
 
